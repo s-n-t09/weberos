1fa0e678133c3eaa04fcfc74f72a9fbddaafc6eb
S.N.T <shikoramy51@outlook.com>
2026-05-03 04:45:01 -0700

    V2.4 changes

--- utils/constants.ts
+++ utils/constants.ts
@@ -12,7 +12,7 @@ export const DEFAULT_FS: FileSystemNode = {
         user: {
           type: 'dir',
           children: {
-            'readme.txt': { type: 'file', content: 'Welcome to WeberOS 2.3!\n\nThis is a fully functional web-based OS.\nTry the new WireBox Browser!' },
+            'readme.txt': { type: 'file', content: 'Welcome to WeberOS!\n\nThis is a fully functional web-based OS.\nTry the new WireBox Browser!' },
             'todo.txt': { type: 'file', content: '- Try WireBox\n- Check Notifications\n- Write a .wbr app with permissions' },
             'hello.wbr': { 
                 type: 'file', 
